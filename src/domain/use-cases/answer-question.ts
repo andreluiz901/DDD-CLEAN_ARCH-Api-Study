49ed18c411b6bcc2e6be35e306eb0351d7ebb4fd
André Luiz <silva.ala82@gmail.com>
2024-01-09 10:59:50 -0300

    feat: implements answers

---
+++ src/domain/use-cases/answer-question.ts
@@ -0,0 +1,24 @@
+import { Answer } from "../entities/answer";
+import { AnswerRepository } from "../repositories/answers-repository";
+
+interface AnswerQuestionUseCaseRequest {
+  instructorId: string;
+  questionId: string;
+  content: string;
+}
+
+export class AnswerQuestionUseCase {
+  constructor(private answersrepository: AnswerRepository) {}
+
+  execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
+    const answer = new Answer({
+      content,
+      authorId: instructorId,
+      questionId,
+    });
+
+    this.answersrepository.create(answer);
+
+    return answer;
+  }
+}
