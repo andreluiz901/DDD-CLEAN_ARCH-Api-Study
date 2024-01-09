985cc27bd40b8e7197b98160f4d1ec8e440a039b
André Luiz <silva.ala82@gmail.com>
2024-01-09 13:26:55 -0300

    change: use UniqueentityID at class entities

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,11 +1,15 @@
 import { Slug } from "./value-objects/slug";
 import { Entity } from "../../core/entities/entity";
+import { UniqueEntityId } from "../../core/entities/unique-entity-id";
 
 interface questionProps {
   title: string;
   content: string;
-  authorId: string;
+  authorId: UniqueEntityId;
   slug: Slug;
+  bestAnswerId?: UniqueEntityId;
+  createdAt: Date;
+  updatedAt?: Date;
 }
 
 export class Question extends Entity<questionProps> {}
