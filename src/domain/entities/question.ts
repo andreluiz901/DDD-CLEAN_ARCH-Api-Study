3cc31e320951e817e446cae6285d570dd6b3114d
André Luiz <silva.ala82@gmail.com>
2024-01-09 11:59:44 -0300

    feat: implements make a slug

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,20 +1,24 @@
 import { randomUUID } from "node:crypto";
+import { Slug } from "./value-objects/slug";
 
 interface questionProps {
   title: string;
   content: string;
   authorId: string;
+  slug: Slug;
 }
 
 export class Question {
   public title: string;
   public content: string;
+  public slug: Slug;
   public id: string;
   public authorId: string;
 
   constructor(props: questionProps, id?: string) {
     this.title = props.title;
     this.content = props.content;
+    this.slug = props.slug;
     this.authorId = props.authorId;
     this.id = id ?? randomUUID();
   }
