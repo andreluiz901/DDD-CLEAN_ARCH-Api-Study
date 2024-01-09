db3ab969f60e82910c0171e62362d351d7266ad0
André Luiz <silva.ala82@gmail.com>
2024-01-09 11:59:18 -0300

    test: ensures that should be able to make a slug

---
+++ src/domain/entities/value-objects/slug.spec.ts
@@ -0,0 +1,9 @@
+import { expect, test } from "vitest";
+import { Slug } from "./slug";
+
+test("it should be able to creeate a new slug from text", () => {
+  const slug = Slug.createFromText("Example text title");
+  console.log(slug);
+
+  expect(slug.value).toBe("example-text-title");
+});
