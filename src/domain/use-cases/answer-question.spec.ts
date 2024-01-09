9af9ec2f4f8e5e1ea6609c442d29292cef9fb52c
André Luiz <silva.ala82@gmail.com>
2024-01-09 11:00:12 -0300

    test: ensures that should be able to answer a question

---
+++ src/domain/use-cases/answer-question.spec.ts
@@ -0,0 +1,22 @@
+import { expect, test } from "vitest";
+import { AnswerQuestionUseCase } from "./answer-question";
+import { AnswerRepository } from "../repositories/answers-repository";
+import { Answer } from "../entities/answer";
+
+const fakeAnswerRepository: AnswerRepository = {
+  create: async (answer: Answer) => {
+    return;
+  },
+};
+
+test("create an answer", async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository);
+
+  const answer = await answerQuestion.execute({
+    content: "nova resposta",
+    instructorId: "1",
+    questionId: "1",
+  });
+
+  expect(answer.content).toEqual("nova resposta");
+});
