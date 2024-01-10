2b96dcf6e84242c86b06b33fe8a239fd7bb0bcf4
André Luiz <silva.ala82@gmail.com>
2024-01-10 19:31:54 -0300

    feat: implements delete a question

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -4,6 +4,24 @@ import { Question } from "@/domain/forum/enterprise/entities/question";
 export class InMemoryQuestionRepository implements QuestionsRepository {
   public items: Question[] = [];
 
+  async findById(id: string) {
+    const question = this.items.find((item) => item.id.toString() === id);
+
+    if (!question) {
+      return null;
+    }
+
+    return question;
+  }
+
+  async delete(question: Question) {
+    const questionIndex = this.items.findIndex(
+      (item) => item.id === question.id
+    );
+
+    this.items.splice(questionIndex, 1);
+  }
+
   async findBySlug(slug: string): Promise<Question | null> {
     const question = this.items.find((item) => item.slug.value === slug);
 
