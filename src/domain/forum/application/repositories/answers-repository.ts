5613f6eb18350b2a0043efe8097d1a15a25812a7
André Luiz <silva.ala82@gmail.com>
2024-01-13 16:15:15 -0300

    feat: implements fetch answers use case

--- src/domain/forum/application/repositories/answers-repository.ts
+++ src/domain/forum/application/repositories/answers-repository.ts
@@ -1,6 +1,11 @@
+import { PaginationParams } from "@/core/repositories/pagination-params";
 import { Answer } from "@/domain/forum/enterprise/entities/answer";
 
 export interface AnswerRepository {
+  findManyByQuestionId(
+    questionId: string,
+    params: PaginationParams
+  ): Promise<Answer[]>;
   findById(id: string): Promise<Answer | null>;
   save(answer: Answer): Promise<void>;
   create(answer: Answer): Promise<void>;
---
+++ src/domain/forum/application/use-cases/fetch-question-answers.ts
@@ -0,0 +1,30 @@
+import { Answer } from "../../enterprise/entities/answer";
+import { Question } from "../../enterprise/entities/question";
+import { AnswerRepository } from "../repositories/answers-repository";
+
+interface FetchQuestionAnswersUseCaseRequest {
+  questionId: string;
+  page: number;
+}
+
+interface FetchQuestionAnswersUseCaseResponse {
+  answers: Answer[];
+}
+
+export class FetchQuestionAnswersUseCase {
+  constructor(private answersRepository: AnswerRepository) {}
+
+  async execute({
+    page,
+    questionId,
+  }: FetchQuestionAnswersUseCaseRequest): Promise<FetchQuestionAnswersUseCaseResponse> {
+    const answers = await this.answersRepository.findManyByQuestionId(
+      questionId,
+      { page }
+    );
+
+    return {
+      answers,
+    };
+  }
+}
