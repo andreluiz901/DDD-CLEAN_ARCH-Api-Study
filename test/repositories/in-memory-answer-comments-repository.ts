15e560058b0ba7fab51bdf5cf139eb8645235fbc
André Luiz <silva.ala82@gmail.com>
2024-01-13 22:00:34 -0300

    feat: implements fetch answer comments use case

--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from "@/core/repositories/pagination-params";
 import { AnswersCommentsRepository } from "@/domain/forum/application/repositories/answer-comments-repository";
 import { Answer } from "@/domain/forum/enterprise/entities/answer";
 import { AnswerComment } from "@/domain/forum/enterprise/entities/answer-comment";
@@ -26,5 +27,13 @@ export class InMemoryAnswerCommentsRepository
     );
 
     this.items.splice(answerCommentIndex, 1);
+  }
+
+  async findManyByAnswerId(answerId: string, { page }: PaginationParams) {
+    const answerComments = this.items
+      .filter((item) => item.answerId.toString() === answerId)
+      .slice((page - 1) * 20, page * 20);
+
+    return answerComments;
   }
 }
