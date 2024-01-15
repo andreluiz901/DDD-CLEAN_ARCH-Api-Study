41c9df2d02648d26c19ab5ba25e83b09770b3040
André Luiz <silva.ala82@gmail.com>
2024-01-15 08:13:29 -0300

    feat: implement domain events classes and interfaces

--- src/core/entities/entity.ts
+++ src/core/entities/entity.ts
@@ -12,4 +12,12 @@ export abstract class Entity<Props> {
     this.props = props;
     this._id = id ?? new UniqueEntityId();
   }
+
+  public equals(entity: Entity<any>) {
+    if (entity === this) return true;
+
+    if (entity.id === this.id) return true;
+
+    return false;
+  }
 }
